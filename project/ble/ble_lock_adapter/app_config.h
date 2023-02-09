3298d675cbad9ea63f50c3393872abfaf81e1cec
hecai <hecai84@gmail.com>
2023-02-09 15:09:28 +0800

    修改定时器数量。

--- project/ble/ble_lock_adapter/app_config.h
+++ project/ble/ble_lock_adapter/app_config.h
@@ -3,10 +3,11 @@
 
 #define SDK_DEEP_SLEEP_ENABLE 0
 
-#define SDK_HCLK_MHZ (64)
+#define SDK_HCLK_MHZ (16)
 
 #define SDK_MAX_PROFILE_NUM 1
 
 #define SDK_MAX_CONN_NUM 5
 
+#define SDK_SW_TIMER_MAX 10
 #endif
